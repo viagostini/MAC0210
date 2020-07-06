b24cf7ffa63b073eb8e788f1a561285c448ecf50
Vinicius Agostini <vinicius.agostini19@gmail.com>
2020-07-06 00:36:21 -0300

    atualizando repo

--- EP3/mc.c
+++ EP3/mc.c
@@ -6,39 +6,35 @@
 typedef double (*function)(double);
 
 double x3 (double x) {
-    return x * x * x;
+    return pow(4*x + 3, 3) * 4;
 }
 
 double e (double x) {
-    return exp(-x);
+    return exp(1 - 1/x) / (x*x);
 }
 
-double random_number() {
-    return (double) rand() / RAND_MAX;
-}
-
-double monte_carlo (int a, int b, int iters, function func) {
+double monte_carlo (int iters, function func) {
     double x, sum = 0;
 
     srand(time(NULL));
 
     for (int i = 0; i < iters; i++) {
-        x = random_number() * (b - a) + a;
+        x = (double) rand() / RAND_MAX;
         sum += func(x);
     }
     
-    return (sum / iters) * (b-a);
+    return sum / iters;
 }
 
 int main() {
     double res;
     
-    res = monte_carlo(0, 1, 1e7, &sin);
+    res = monte_carlo(1e7, &sin);
     printf("%f\n", res);
 
-    res = monte_carlo(3, 7, 1e7, &x3);
+    res = monte_carlo(1e7, &x3);
     printf("%f\n", res);
-   
-    res = monte_carlo(0, 9e3, 1e7, &e);
+
+    res = monte_carlo(1e7, &e);
     printf("%f\n", res);
 }
--- EP3/trapezoidal.c
+++ EP3/trapezoidal.c
@@ -1,21 +1,96 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-double trapezoidal (int n, double f[], double step) {
-    double sum = 0.5 * (f[0] + f[n-1]);
+typedef struct Point {
+    double x, y;
+} Point;
 
-    for (int i = 0; i < n-1; i++)
-        sum += f[i];
+#ifdef CAPZERO
+    int CAP_ZERO = 1;
+#else
+    int CAP_ZERO = 0;
+#endif
+
+double interpolate (Point points[], double xi, int n) { 
+    double result = 0;
+  
+    for (int i = 0; i < n; i++) { 
+        double term = points[i].y;
+
+        for (int j = 0; j < n; j++) { 
+            if (j==i) continue;
+            term *= (xi - points[j].x) / (points[i].x - points[j].x); 
+        }  
+        result += term; 
+    }
+    
+    return (result < 0 && CAP_ZERO) ? 0 : result;
+} 
+
+Point *interpolate_interval (Point points[], int sz, int a, int b, int n, double *step) {
+    *step = ((double)b - a) / (n-1);
+    printf("step = %f\n", *step);
+    Point *res = (Point*) calloc(n, sizeof(Point));
+
+    for (int i = 0; i < n; i++) {
+        Point new_point = {a + i*(*step), interpolate(points, a + i*(*step), sz)};
+        res[i] = new_point;
+    }
+    
+    return res;
+}
+
+double trapezoidal (int n, Point points[], double step) {
+    double sum = 0.5 * (points[0].y + points[n-1].y);
+
+    for (int i = 1; i < n-1; i++)
+        sum += points[i].y;
 
     return step * sum;
 }
 
-int main () {
-    int n = 7;
-    int step = 5;
-    double fcos[] = {0, 1.5297, 9.5120, 8.7025, 2.8087, 1.0881, 0.3537};
+int main (int argc, char **argv) {
+    int a = 0, b = 30, n = 7, new_a, new_b, new_n;
 
-    double result = trapezoidal(n, fcos, step);
+    if (argc > 4) {
+        printf("Uso: ./trapezoidal a b n\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    if (argc == 1)
+        new_a = 0, new_b = 30, new_n = 7;
+    else
+        new_a = atoi(argv[1]), new_b = atoi(argv[2]), new_n = atoi(argv[3]);
 
+
+
+
+    double step = 5.0;
+    Point points[] = {
+        {0, 0},
+        {5, 1.5297},
+        {10, 9.5120},
+        {15, 8.7025},
+        {20, 2.8087},
+        {25, 1.0881},
+        {30, 0.3537}
+    }; 
+
+    double new_step;
+    Point *interpolated = interpolate_interval(points, n, new_a, new_b, new_n, &new_step);
+
+
+    for (int i = 0; i < new_n; i++) {
+        printf("(%.4f, %.4f) ", interpolated[i].x, interpolated[i].y);
+    }
+    printf("\n\n");
+
+    double result = trapezoidal(n, points, step);
     printf("Result is %f\n", result);
+
+    result = trapezoidal(new_n, interpolated, new_step);
+    printf("Result with interpolation is %f\n", result);
+
+    free(interpolated);
 
 }
